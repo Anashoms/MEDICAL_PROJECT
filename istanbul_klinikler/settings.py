d34f1327e3d0b65bf74808e180563a85728cf972
Anas Daas <anasdaas210@gmail.com>
2024-10-30 13:55:05 +0300

    new apps

--- istanbul_klinikler/settings.py
+++ istanbul_klinikler/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'patients',
+    'userprofile',
+    'doctors',
+    'clinics',
+    'appointments',
+    
 ]
 
 MIDDLEWARE = [
